fa547798abe189c428db6f33bda82d5eddccd3ff
Fahrenhei7 <fahrenhei7lt@gmail.com>
2016-08-05 16:57:22 +0300

    another rubocop refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   include Pundit
 
   protect_from_forgery with: :exception
@@ -11,8 +10,8 @@ class ApplicationController < ActionController::Base
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
 
-    flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    flash[:error] = t "#{policy_name}.#{exception.query}", scope: 'pundit',
+                                                           default: :default
     redirect_to(request.referrer || quests_path)
   end
-
 end
--- app/controllers/web/quests/application_controller.rb
+++ app/controllers/web/quests/application_controller.rb
@@ -1,9 +1,7 @@
 class Web::Quests::ApplicationController < Web::ApplicationController
-
   helper_method :current_quest
 
   def current_quest
     @current_quest ||= Quest.find(params[:quest_id])
   end
-
 end
