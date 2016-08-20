7e6893076ae210932cf47e621d0d1cd32dca32d5
Fahrenhei7 <fahrenhei7lt@gmail.com>
2016-08-20 07:14:39 +0300

    views chenges for admin / owner

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,5 +1,16 @@
 module MissionsHelper
-  def owner_of_mission(user, mission)
-    mission.quest.creator == user
+  #def owner_of_mission(user, mission)
+  #mission.quest.creator == user
+  #end
+  def status_label(status)
+    label_class = case status
+                  when 'solved'
+                    'success'
+                  when 'active'
+                    'primary'
+                  when 'locked'
+                    'danger'
+                  end
+    "<span class='label label-#{label_class}'> #{status} </span>"
   end
 end
